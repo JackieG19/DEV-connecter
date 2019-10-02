be06b1671e35e89bb2b51d5daed0ed07de10fe7a
Jackie <JGtheophila19@gmail.com>
2019-10-02 11:07:29 -0400

    creating cards with suits and, displaying it

--- finishLine.cs
+++ finishLine.cs
@@ -36,6 +36,47 @@ public class Die
 	}
 }
 
+public class Card
+{
+ 	public int suit;
+ 	public int val;
+ 	private Dictionary<int, string> SUIT_MAP = new Dictionary<int, string> {
+ 		{0, "\u2663"},
+ 		{1, "\u2660"},
+ 		{2, "\u2665"},
+ 		{3, "\u2666"}
+ 	};
+ 	private Dictionary<int, string> VAL_MAP = new Dictionary<int, string> {
+ 		{1, "Ac"},
+ 		{10, "10"},
+ 		{11, "Ja"},
+ 		{12, "Qu"},
+ 		{13, "Ki"}
+ 	};
+
+ 	public Card(int val, int suit)
+ 	{
+ 		this.val = val;
+ 		this.suit = suit;
+ 	}
+
+ 	public string Display()
+ 	{
+ 		if (this.val == 0)
+ 		{
+ 			return "Jkr";
+ 		}
+
+ 		if (this.VAL_MAP.ContainsKey(this.val))
+ 		{
+ 			return this.SUIT_MAP[this.suit] + this.VAL_MAP[this.val];
+ 		}
+
+ 		return this.SUIT_MAP[this.suit] + "0" + this.val;
+
+ 	}
+
+
 public class Marker
 {
  	public int position;
